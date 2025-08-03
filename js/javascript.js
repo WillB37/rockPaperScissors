d5627661253fcd3e8cd849291454708d81c999cd
Will Ball <ballwill1@gmail.com>
2025-08-03 13:38:08 -0700

    Add in pseudocode

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+// Write function called getComputerChoice
+// It will return, at random, "rock" "paper" "scissors"
+//
+// Write function called getHumanChoice
+// User picks between "rock" "paper" "scissors"
+// No need to handle invalid choices
+//
+// "humanScore" and "computerScore" will keep track of scores
+//
+// Write function call playRound
+// Plays a single round
+// Stores choices in "humanChoice" and "computerChoice"
+// "humanChoice" is case-insensitive
+// outputs to console.log
+// increments scores
+//
+// Write playGame function
+// Plays 5 rounds
