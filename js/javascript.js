3af433d5e72750b04165e9d72efc16003da67c4a
Will Ball <ballwill1@gmail.com>
2025-08-13 18:37:35 -0700

    Add in computer choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,20 @@
 // Write function called getComputerChoice
 // It will return, at random, "rock" "paper" "scissors"
-//
+
+function getComputerChoice() {
+  let choice = Math.random();
+
+  if ( choice <= 1/3 ) {
+    return("rock");
+  } else if ( choice > 1/3 && choice <= 2/3 ) {
+    return("paper");
+  } else {
+    return ("scissors");
+  }
+}
+
+console.log(getComputerChoice());
+
 // Write function called getHumanChoice
 // User picks between "rock" "paper" "scissors"
 // No need to handle invalid choices
