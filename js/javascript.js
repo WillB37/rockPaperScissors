9b3410b78b41e2aac31ef9f04e76f7a5b7de89d6
Will Ball <ballwill1@gmail.com>
2025-08-13 19:09:38 -0700

    Add in play round

--- js/javascript.js
+++ js/javascript.js
@@ -6,16 +6,14 @@ let computerScore = 0;
 function getComputerChoice() {
   let choice = Math.random();
 
-  if ( choice <= 1/3 ) {
-    return("rock");
-  } else if ( choice > 1/3 && choice <= 2/3 ) {
-    return("paper");
+  if (choice <= 1 / 3) {
+    return "rock";
+  } else if (choice > 1 / 3 && choice <= 2 / 3) {
+    return "paper";
   } else {
-    return ("scissors");
+    return "scissors";
   }
 }
-
-console.log(getComputerChoice());
 
 // User picks between "rock" "paper" "scissors"
 // No need to handle invalid choices
@@ -24,16 +22,35 @@ function getHumanChoice() {
   return prompt("Enter Rock, Paper, or Scissors");
 }
 
-console.log(getHumanChoice())
-
-// "humanScore" and "computerScore" will keep track of scores
-//
 // Write function call playRound
 // Plays a single round
 // Stores choices in "humanChoice" and "computerChoice"
 // "humanChoice" is case-insensitive
 // outputs to console.log
 // increments scores
-//
+
+function playRound() {
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice().toLowerCase();
+
+  if (computerChoice === humanChoice) {
+    console.log(`Tie! ${computerChoice} equals ${humanChoice}.`);
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    computerScore++;
+  } else {
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}.`);
+    humanScore++;
+  }
+}
+
+playRound();
+console.log(`Computer Score: ${computerScore}`);
+console.log(`Human Score: ${humanScore}`);
+
 // Write playGame function
 // Plays 5 rounds
